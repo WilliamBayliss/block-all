cc710328b14e51e984cddbd9c53ebd8b4b7c4df4
williambayliss <wmbayliss@gmail.com>
2021-11-17 17:29:01 -0500

    will correctly seed db with values from hardcoded variable, will manually add some accounts later

--- seed_db.js
+++ seed_db.js
@@ -2,19 +2,40 @@ require('dotenv').config('./.env')
 const { MongoClient } = require('mongodb');
 const puppeteer = require('puppeteer');
 
-async function main() {
+const twitterAccounts = {
+    "TIDAL" : "https://twitter.com/TIDAL"
+}
+
+
+async function seedDB() {
     // TODO
     const url = process.env.MONGO_URL;
-
     const client = new MongoClient(url);
-    console.log("Connected!");
     try {
         await client.connect();
+        console.log('Connected!');
+        const accounts = client.db('blockAll').collection('accounts');
+        accounts.drop();
+        accountsToPush = [];
+        for (let account in twitterAccounts) {
+
+            let accountInDB = {
+                name: account,
+                twitterURL: twitterAccounts[account]
+            }
+
+            accountsToPush.push(accountInDB);
+        }
+
+        accounts.insertMany(accountsToPush)
+
+        console.log("Database seeded!")
+
     } catch (e) {
         console.error(e);
     } finally {
-        await client.close();
+       await client.close();
     }
 }
 
-main().catch(console.error);
+seedDB();
